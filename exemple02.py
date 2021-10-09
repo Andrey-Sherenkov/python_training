b63e2aa863aaa85052a928c6fd931c23cdc4a8e7
Andrey-Sherenkov <89512821+Andrey-Sherenkov@users.noreply.github.com>
2021-10-09 23:17:24 +0600

    Update exemple02.py

--- exemple02.py
+++ exemple02.py
@@ -1,8 +1,15 @@
 # 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 # Используйте форматирование строк.
+while True:
+    try:
+        time_sec = int(input('Введите число секунд >>>'))
+        break
+    except ValueError:
+        print("Oops! It doesn't look like a number!")
 
-time_sec = int(input('Введите число секунд >>>'))
 
-print(time_sec // 3600, "часов")
-print((time_sec % 3600) // 60, "минут")
-print((time_sec % 3600) %60, "секунд")
+seconds = time_sec % 60
+minutes = (time_sec // 60) % 60
+hours = (time_sec // 60) // 60
+print(f"{hours}:{minutes}:{seconds}")
+print(f'{hours:>02}:{minutes:>02}:{seconds:>02}')
