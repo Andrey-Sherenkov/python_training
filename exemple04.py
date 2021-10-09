6373494887ec799d74c375e9a6232a8e29d1dd69
Andrey-Sherenkov <89512821+Andrey-Sherenkov@users.noreply.github.com>
2021-10-09 23:36:45 +0600

    Update exemple04.py

--- exemple04.py
+++ exemple04.py
@@ -1,14 +1,16 @@
 # 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
+while True:
+    try:
+        number = int(input('Введите целое положительное число: '))
+        if number > 0:
+            break
+    except ValueError:
+        print("Oops! It doesn't look like a number!")
 
-number = int(input('Введите целое положительное число: '))
 max_num = 0
 
-if number < 0:
-    print("надо целое положительное число")
-    exit(1)
 while number > 0:
-    print(number % 10)
     if (number % 10) > max_num:
         max_num = number % 10
     number = number // 10
