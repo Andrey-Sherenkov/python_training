b5dc62af15f4930f5765e121638f3be5cb31e9fb
Andrey-Sherenkov <89512821+Andrey-Sherenkov@users.noreply.github.com>
2021-10-16 12:58:13 +0600

    Add files via upload

---
+++ func_02.py
@@ -0,0 +1,15 @@
+# 2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+# имя, фамилия, год рождения, город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
+
+my_list = ["иван",  "иванов", "1999", "omsk", "iii@mail.ru", 899999]
+
+
+def my_func(first_name, last_name, year_of_birth, citi, email, phone):
+    print(
+        f"first_name - {first_name} last_name - {last_name} year_of_birth {year_of_birth} citi-{citi} email {email}  phone:{phone}")
+    return 0
+
+
+my_func(*my_list)
